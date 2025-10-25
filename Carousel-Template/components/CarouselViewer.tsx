e46437cff3b131f8af2ac8e4c5233cb7e5c75adb
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2025-10-25 16:44:46 -0300

    Updated CarouselViewer.tsx

--- Carousel-Template/components/CarouselViewer.tsx
+++ Carousel-Template/components/CarouselViewer.tsx
@@ -927,7 +927,10 @@ const CarouselViewer: React.FC<CarouselViewerProps> = ({ slides, carouselData, o
 
   /** ====================== Render ======================= */
   return (
-    <div className="fixed top-14 left-16 right-0 bottom-0 z-[90] bg-neutral-900 flex">
+      <div
+        className="fixed top-14 left-16 right-0 bottom-0 bg-neutral-900 flex"
+        style={{ zIndex: 99 }}
+      >
       {/* Modal */}
       {imageModal.open && (
         <ModalPortal>
